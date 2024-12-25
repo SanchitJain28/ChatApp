96d7703a2c1c69dfac88b09ea4b15532b9311122
SanchitJain28 <mythichuman28@gmail.com>
2024-12-25 15:45:28 +0530

    "CLOUD DATABSE UPLOADED"

--- backend/Database.js
+++ backend/Database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 main().catch(err => console.log(err));
 
 export default async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/ChatApp');
+  await mongoose.connect('mongodb+srv://sanchitjain00028:WpoaDGqlxSPkNmk0@cluster0.738gr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log("ChatApp database is connected")
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
